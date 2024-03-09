7f4afc788604f73a2226dacc80c3a04f0cccf0bc
chauttm <chauttm.vnu@gmail.com>
2024-03-09 22:59:55 +0700

    font & text

---
+++ 10_text/graphics.h
@@ -0,0 +1,127 @@
+#ifndef _GRAPHICS__H
+#define _GRAPHICS__H
+
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include "defs.h"
+
+struct Graphics {
+    SDL_Renderer *renderer;
+	SDL_Window *window;
+
+	void logErrorAndExit(const char* msg, const char* error)
+    {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "%s: %s", msg, error);
+        SDL_Quit();
+    }
+
+	void init() {
+        if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+            logErrorAndExit("SDL_Init", SDL_GetError());
+
+        window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+        //full screen
+        //window = SDL_CreateWindow(WINDOW_TITLE, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_FULLSCREEN_DESKTOP);
+        if (window == nullptr) logErrorAndExit("CreateWindow", SDL_GetError());
+
+        if (!IMG_Init(IMG_INIT_PNG | IMG_INIT_JPG))
+            logErrorAndExit( "SDL_image error:", IMG_GetError());
+
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+        //Khi chạy trong máy ảo (ví dụ phòng máy ở trường)
+        //renderer = SDL_CreateSoftwareRenderer(SDL_GetWindowSurface(window));
+
+        if (renderer == nullptr) logErrorAndExit("CreateRenderer", SDL_GetError());
+
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+        SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+        if (TTF_Init() == -1) {
+            logErrorAndExit("SDL_ttf could not initialize! SDL_ttf Error: ", TTF_GetError());
+        }
+    }
+
+	void prepareScene(SDL_Texture * background)
+    {
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy( renderer, background, NULL, NULL);
+    }
+
+    void presentScene()
+    {
+        SDL_RenderPresent(renderer);
+    }
+
+    SDL_Texture *loadTexture(const char *filename)
+    {
+        SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_INFO, "Loading %s", filename);
+
+        SDL_Texture *texture = IMG_LoadTexture(renderer, filename);
+        if (texture == NULL)
+            SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load texture %s", IMG_GetError());
+
+        return texture;
+    }
+
+    void renderTexture(SDL_Texture *texture, int x, int y)
+    {
+        SDL_Rect dest;
+
+        dest.x = x;
+        dest.y = y;
+        SDL_QueryTexture(texture, NULL, NULL, &dest.w, &dest.h);
+
+        SDL_RenderCopy(renderer, texture, NULL, &dest);
+    }
+
+    void blitRect(SDL_Texture *texture, SDL_Rect *src, int x, int y)
+    {
+        SDL_Rect dest;
+
+        dest.x = x;
+        dest.y = y;
+        dest.w = src->w;
+        dest.h = src->h;
+
+        SDL_RenderCopy(renderer, texture, src, &dest);
+    }
+
+    void quit()
+    {
+        TTF_Quit();
+        IMG_Quit();
+
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+    }
+
+    TTF_Font* loadFont(const char* path, int size)
+    {
+        TTF_Font* gFont = TTF_OpenFont( path, size );
+        if (gFont == nullptr) {
+            SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Load font %s", TTF_GetError());
+        }
+    }
+
+    SDL_Texture* renderText(const char* text, TTF_Font* font, SDL_Color textColor)
+    {
+        SDL_Surface* textSurface = TTF_RenderText_Solid( font, text, textColor );
+        if( textSurface == nullptr ) {
+            SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Render text surface %s", TTF_GetError());
+            return nullptr;
+        }
+
+        SDL_Texture* texture = SDL_CreateTextureFromSurface( renderer, textSurface );
+        if( texture == nullptr ) {
+            SDL_LogMessage(SDL_LOG_CATEGORY_APPLICATION, SDL_LOG_PRIORITY_ERROR, "Create texture from text %s", SDL_GetError());
+        }
+
+        SDL_FreeSurface( textSurface );
+        return texture;
+    }
+};
+
+#endif // _GRAPHICS__H
---
+++ 10_text/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_ttf.h>
+#include "graphics.h"
+#include "defs.h"
+
+using namespace std;
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if ( SDL_PollEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    Graphics graphics;
+    graphics.init();
+
+    TTF_Font* font = graphics.loadFont("assets/Purisa-BoldOblique.ttf", 100);
+
+    SDL_Color color = {255, 255, 0, 0};
+    SDL_Texture* helloText = graphics.renderText("Hello", font, color);
+
+    graphics.renderTexture(helloText, 200, 200);
+
+    graphics.presentScene();
+    waitUntilKeyPressed();
+
+	SDL_DestroyTexture( helloText );
+    helloText = NULL;
+
+    graphics.quit();
+    return 0;
+}
